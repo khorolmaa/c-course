6ff7207bed65e94e40d41b8e1bf9c31b8e01fe2e
khorolmaa <80461679+khorolmaa@users.noreply.github.com>
2021-03-12 00:03:38 -0800

    Add files via upload

---
+++ HW1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int x,y,z,vil,vild,vildel;
+	x = 12;
+	y = 60;
+	z = 8;
+    vil = x+y-z;
+	vild = x/z*y;
+    vildel = 20*x*(z+y);
+	
+	
+	cout<<"x+y-z="<<vil<<endl<<"x/z*y="<<vild<<endl<<"20*x*(z+y)="<<vildel<<endl;
+	
+	return 0;
+}

06abb4df56a2bc619716a2b5a5cee55b64554da1
khorolmaa <80461679+khorolmaa@users.noreply.github.com>
2021-03-18 11:22:04 -0700

    Add files via upload

---
+++ HW2.2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	double x,y;
+	
+	cout<<"x=";
+	cin>>x;
+	cout<<"y=";
+	cin>>y;
+	cout<<"x+y="<<x+y<<endl<<"x*y="<<x*y<<endl<<"x/y="<<x/y<<endl<<"x-y="<<x-y<<endl;
+	
+	return 0;
+}

301aeaee9477013239195d22694a4901c937da84
khorolmaa <80461679+khorolmaa@users.noreply.github.com>
2021-03-18 11:30:23 -0700

    Add files via upload

---
+++ HW2.4.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int a,b;
+	cout<<"a=";
+	cin>>a;
+	cout<<"b=";	
+	cin>>b;
+	cout<<"S="<<a*b<<endl<<"P="<<(a+b)*2<<endl; //tegsh ontsogtiin S,P oloh
+	return 0;
+}
